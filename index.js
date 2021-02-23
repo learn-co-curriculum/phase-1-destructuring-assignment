b1a0a4a0166143299262a4bf9ba8158795eea9e4
Liz Burton <liz.burton147@gmail.com>
2021-02-23 11:16:55 -0500

    update starter code and instructions

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const farmAnimals = 'cow horse sheep pig chicken'
+const farmAnimals = 'cow horse sheep pig chicken';
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 const muppet = {
-  name: 'Kermit',
-  color: 'green',
-  song: 'The Rainbow Connection',
-  job: 'Host of The Muppet Show',
-  partner: 'Miss Piggy'
+  muppetName: 'Miss Piggy',
+  color: 'pink',
+  song: 'Never Before, Never Again',
+  job: 'Cast member of The Muppet Show',
+  partner: 'Kermit'
 };
 
-const k_muppet = {
-  k_name: 'Kermit',
-  k_color: 'green',
-  k_album: {
-    the_muppet_movie: {
-      song_1: 'Rainbow Connection',
-      song_2: 'Moving Right Along',
-      song_3: 'Never Before, Never Again',
-      song_4: 'I Hope That Something Better Comes Along',
+const nestedMuppet = {
+  nestedName: 'Kermit',
+  nestedColor: 'green',
+  album: {
+    theMuppetMovie: {
+      song1: 'Rainbow Connection',
+      song2: 'Moving Right Along',
+      song3: 'Never Before, Never Again',
+      song4: 'I Hope That Something Better Comes Along',
     },
   },
-  k_job: 'Host of The Muppet Show',
-  k_partner: 'Miss Piggy'
+  nestedJob: 'Host of The Muppet Show',
+  nestedPartner: 'Miss Piggy'
 };
 
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
 
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
+// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
 
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
+// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
 
 // Arrays
 
@@ -43,7 +43,6 @@ const k_muppet = {
 
 // Objects
 
-// 7. Use destructuring to assign all appropriate variables using the keys as the variable names
-
+// 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
